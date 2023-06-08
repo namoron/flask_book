9bbe214d9ed83839c16993e6ab3d84fb086416ad
namor <n2210478@gl.cc.uec.ac.jp>
2023-06-08 16:29:40 +0900

    p49

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return 'Hello,Flaskbook!'
 
-@app.route("/hello")
-def hello():
-    return "Hello, World!" 
+@app.route("/hello/<name>",methods=["GET"],endpoint="hello-endpoint")
+def hello(name):
+    return f"Hello,{name}!"
+
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html",name = name) 
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint",name= "world"))
+    # /name/mana?page=1
+    print(url_for("show_name",name= "mana",page="1"))
