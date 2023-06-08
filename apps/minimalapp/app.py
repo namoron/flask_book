9d1a4b00d0a0fd1691c8900aadad04c04224c770
namor <n2210478@gl.cc.uec.ac.jp>
2023-06-08 17:17:09 +0900

    p52

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,current_app,g,request
 
 app = Flask(__name__)
 
@@ -21,3 +21,7 @@ with app.test_request_context():
     print(url_for("hello-endpoint",name= "world"))
     # /name/mana?page=1
     print(url_for("show_name",name= "mana",page="1"))
+
+with app.test_request_context("/users?updated=true"):
+    # /
+    print(request.args.get("updated"))
