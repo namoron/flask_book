926b0939cb71e5aa24a94472a57d3ace9cdcff0c
namor <n2210478@gl.cc.uec.ac.jp>
2023-06-13 21:16:06 +0900

    p64

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,url_for,current_app,g,request
-
+from email_validator import validate_email,EmailNotValidError
+from flask import Flask,render_template,url_for,current_app,g,request,redirect,flash
 app = Flask(__name__)
+# SECRET_KEYを追加
+app.config["SECRET_KEY"]  = "2AZSMss3p5QPbcY2hBsJ"
 
 @app.route("/")
 def index():
@@ -22,6 +24,58 @@ with app.test_request_context():
     # /name/mana?page=1
     print(url_for("show_name",name= "mana",page="1"))
 
+#アプリケーションコンテキストを取得してスタックへpush
+ctx = app.app_context()
+ctx.push()
+
+print(current_app.name)
+
+g.connection = "connection"
+print(g.connection)
+
+
+
 with app.test_request_context("/users?updated=true"):
     # /
     print(request.args.get("updated"))
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete",methods=["GET","POST"])
+def contact_complete():
+    if request.method == "POST":
+        # form属性を使ってフォームの値を取得
+        username = request.form["username"]
+        email  = request.form["email"]
+        description  = request.form["description"]
+        #入力チェック
+        is_valid = True
+        
+        if not username:
+            flash("ユーザー名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+            
+        if not is_valid:
+            return redirect(url_for("contact"))
+        # メールを送る
+
+        #問い合わせ完了エンドポイントへリダイレクト
+        flash("問い合わせ内容はメールにて送信しました．問い合わせありがとうございました．")
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
