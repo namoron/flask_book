9999839abd23ec93057c2ae86a2ca3606546ebf3
namor <n2210478@gl.cc.uec.ac.jp>
2023-06-13 23:27:11 +0900

    p68

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,9 +1,17 @@
 from email_validator import validate_email,EmailNotValidError
 from flask import Flask,render_template,url_for,current_app,g,request,redirect,flash
+import logging
+from flask_debugtoolbar import DebugToolbarExtension
+
 app = Flask(__name__)
 # SECRET_KEYを追加
 app.config["SECRET_KEY"]  = "2AZSMss3p5QPbcY2hBsJ"
-
+#ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+#リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INSTERCEPT_REDIRECTS"]  = False
+# DebugToolbarExtension にアプリケーションをセットする
+toolbar  =  DebugToolbarExtension(app)
 @app.route("/")
 def index():
     return 'Hello,Flaskbook!'
@@ -70,7 +78,7 @@ def contact_complete():
         if not description:
             flash("問い合わせ内容は必須です")
             is_valid = False
-            
+
         if not is_valid:
             return redirect(url_for("contact"))
         # メールを送る
